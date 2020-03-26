d5ec5c275057d21874f09531307feb5a06db370e
Tadahiro Kato <goemon12.tc4@gmail.com>
2020-03-26 19:51:53 +0900

    大幅変更Canvas使用

--- Life.js
+++ Life.js
@@ -1,44 +1,38 @@
 let MAP_H = 10;
 let MAP_W = 20;
+let MAP;
 
-let map1;
-let map2;
+function disp() {
+    let color = [
+        "#ff0000",
+        "#00ff00"
+    ]
 
-function dispM() {
-    let td = document.getElementsByTagName("td");
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext("2d");
 
     for (let i = 0; i < MAP_H; i++) {
         for (let j = 0; j < MAP_W; j++) {
-            td[i * MAP_W + j].innerText = map1[i][j];
+            context.fillStyle = color[MAP[i][j]];
+            context.beginPath();
+            context.arc(j * 10 + 5, i * 10 + 5, 2, 0, Math.PI * 2, true);
+            context.fill();
         }
     }
 }
 
 window.onload = function() {
-    let table = document.getElementsByTagName("table");
-
-    map1 = new Array(MAP_H);
-    map2 = new Array(MAP_H);
-
+    MAP = new Array(MAP_H);
     for (let i = 0; i < MAP_H; i++) {
-
-        let tr = document.createElement("tr");
-        table[0].appendChild(tr);
-
-        map1[i] = new Array(MAP_W);
-        map2[i] = new Array(MAP_W);
-
+        MAP[i] = new Array(MAP_W);
         for (let j = 0; j < MAP_W; j++) {
-
-            let td = document.createElement("td");
-            tr.appendChild(td);
-
-            map1[i][j] = 0;
-            map2[i][j] = 0;
+            MAP[i][j] = 0;
         }
     }
 
-    map1[5][10] = 1;
+    let y1 = Math.floor(Math.random() * MAP_H);
+    let x1 = Math.floor(Math.random() * MAP_W);
+    MAP[y1][x1] = 1;
 
-    dispM();
+    disp();
 }
